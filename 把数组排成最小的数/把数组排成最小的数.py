667a3f262607850c5aa645fc7b31f2c8d75b74c7
QunLeLZ <a9276985676@qq.com>
2018-11-13 20:34:08 +0800

    first commit

---
+++ 把数组排成最小的数/把数组排成最小的数.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+class Solution:
+    def PrintMinNumber(self, numbers):
+        # write code here
+        if not numbers:
+            return ""
+        numbers = list(map(str,numbers))
+        numbers.sort(cmp=lambda x, y: cmp(x + y, y + x)) 
+        return "".join(numbers)
