a20ab6c8ed5f0a1b8a30758790fd2f451282e283
QunLeLZ <a9276985676@qq.com>
2018-11-27 20:33:38 +0800

    first commit

---
+++ LeetCode/两数相加/两数相加.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int n = 0;
+        ListNode l3 = new ListNode(0);
+        ListNode p = l1,q = l2,curr = l3;
+        while(p != null || q != null){
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum = n + x + y;
+            n = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if(p != null) p = p.next;
+            if(q != null) q = q.next;
+        }
+        if(n > 0){
+            curr.next = new ListNode(n);
+        }
+        return l3.next;
+    }
+}
